b11073e6dc09edf879e6acdc16564527707a2777
Sumukha21 <45757780+Sumukha21@users.noreply.github.com>
2023-12-09 14:28:40 -0700

    Basic logic of TSConv added and integrated

--- unet.py
+++ unet.py
@@ -1,43 +1,51 @@
 import torch
-from unet_utils import DownsampleBlock, UpsampleBlock
+from unet_utils import DownsampleBlock, UpsampleBlock, TSConv
 
 
 class Unet(torch.nn.Module):
-    def __init__(self, input_channel_size, conv_kernel_size, encoder_channels_list, decoder_channels_list):
+    def __init__(self, encoder_channels_list, decoder_channels_list, conv_type="basic"):
         """
         Define the layer blocks since structure is going to remain the same
         """
         super(Unet, self).__init__()
-        self.conv_input1 = torch.nn.Conv2d(in_channels=input_channel_size,
+        self.conv_input1 = torch.nn.Conv2d(in_channels=1,
                                            out_channels=encoder_channels_list[0],
-                                           kernel_size=conv_kernel_size,
+                                           kernel_size=(3, 3),
                                            stride=(1, 1),
                                            padding="same")
-        self.conv_input2 = torch.nn.Conv2d(in_channels=encoder_channels_list[0],
-                                           out_channels=encoder_channels_list[0],
-                                           kernel_size=conv_kernel_size,
-                                           stride=(1, 1),
-                                           padding="same")
+        if conv_type == "basic":
+            self.conv_input2 = torch.nn.Conv2d(in_channels=encoder_channels_list[0],
+                                               out_channels=encoder_channels_list[0],
+                                               kernel_size=(3, 3),
+                                               stride=(1, 1),
+                                               padding="same")
+            self.final_conv = torch.nn.Conv2d(in_channels=decoder_channels_list[-1],
+                                              out_channels=1,
+                                              kernel_size=(3, 3),
+                                              stride=(1, 1),
+                                              padding="same")
+        
+        elif conv_type == "time_shifted":
+            self.conv_input2 = TSConv(input_channels=encoder_channels_list[0],
+                                      output_channels=encoder_channels_list[0])
+            self.final_conv = TSConv(input_channels=decoder_channels_list[-1],
+                                      output_channels=1)
+        else:
+            raise NotImplementedError("""Right now only two types of convolution layers 
+                                      are supported 'basic' and 'time_shifted'.
+                                      Read the class docstring for more info.""")
         self.encoder_blocks = []
         input_conv_channel_size = encoder_channels_list[0]
         for i in encoder_channels_list[1:]:
             self.encoder_blocks.append(DownsampleBlock(conv_in_channels=input_conv_channel_size,
-                                                       conv_out_channels=i,
-                                                       conv_kernel_size=conv_kernel_size))
+                                                       conv_out_channels=i, conv_type=conv_type))
             input_conv_channel_size = i
         self.decoder_blocks = []
         input_conv_channel_size = encoder_channels_list[-1]
         for i in range(len(decoder_channels_list)):
             self.decoder_blocks.append(UpsampleBlock(conv_in_channels=input_conv_channel_size + encoder_channels_list[~(i+1)],
-                                                     conv_out_channels=decoder_channels_list[i],
-                                                     conv_kernel_size=conv_kernel_size))
+                                                     conv_out_channels=decoder_channels_list[i], conv_type=conv_type))
             input_conv_channel_size = decoder_channels_list[i]
-        self.final_conv = torch.nn.Conv2d(in_channels=decoder_channels_list[-1],
-                                          out_channels=1,
-                                          kernel_size=conv_kernel_size,
-                                          stride=(1, 1),
-                                          padding="same")
-        
     
     def forward(self, x):
         """
@@ -55,4 +63,11 @@ class Unet(torch.nn.Module):
             x = self.decoder_blocks[i](x, encoder_output_to_concat)
         x = self.final_conv(x)
         return x
+
+"""
+ToDO:
+1. Add assert statements to verify length of encoder and decoder channel lists
+2. Remove input_channel_size and kernel_size as parameters - Done
+3. Add assert statement to verify the values of encoder, decoder channel values
+"""
 
--- unet_utils.py
+++ unet_utils.py
@@ -2,19 +2,29 @@ import torch
 
 
 class DownsampleBlock:
-    def __init__(self, conv_in_channels, conv_out_channels, conv_kernel_size):
+    def __init__(self, conv_in_channels, conv_out_channels, conv_type="basic"):
         self.maxpool = torch.nn.MaxPool2d(kernel_size=(2, 1))
-        self.conv_layer1 = torch.nn.Conv2d(in_channels=conv_in_channels,
-                                          out_channels=conv_out_channels,
-                                          kernel_size=conv_kernel_size,
-                                          stride=(1, 1),
-                                          padding="same")
-        self.conv_layer2 = torch.nn.Conv2d(in_channels=conv_out_channels,
-                                          out_channels=conv_out_channels,
-                                          kernel_size=conv_kernel_size,
-                                          stride=(1, 1),
-                                          padding="same")
-    
+        if conv_type == "basic":
+            self.conv_layer1 = torch.nn.Conv2d(in_channels=conv_in_channels,
+                                               out_channels=conv_out_channels,
+                                               kernel_size=(3, 3),
+                                               stride=(1, 1),
+                                               padding="same")
+            self.conv_layer2 = torch.nn.Conv2d(in_channels=conv_out_channels,
+                                               out_channels=conv_out_channels,
+                                               kernel_size=(3, 3),
+                                               stride=(1, 1),
+                                               padding="same")
+        elif conv_type == "time_shifted":
+            self.conv_layer1 = TSConv(input_channels=conv_in_channels,
+                                      output_channels=conv_out_channels)
+            self.conv_layer2 = TSConv(input_channels=conv_out_channels,
+                                      output_channels=conv_out_channels)
+        else:
+            raise NotImplementedError("""Right now only two types of convolution layers 
+                                      are supported 'basic' and 'time_shifted'.
+                                      Read the class docstring for more info.""")
+
     def __call__(self, x):
         x = self.maxpool(x)
         x = torch.nn.ReLU()(self.conv_layer1(x))
@@ -23,18 +33,28 @@ class DownsampleBlock:
 
 
 class UpsampleBlock:
-    def __init__(self, conv_in_channels, conv_out_channels, conv_kernel_size):        
+    def __init__(self, conv_in_channels, conv_out_channels, conv_type="basic"):        
         self.upsample = torch.nn.Upsample(scale_factor=(2, 1))
-        self.conv_layer1 = torch.nn.Conv2d(in_channels=conv_in_channels,
-                                           out_channels=conv_out_channels,
-                                           kernel_size=conv_kernel_size,
-                                           stride=(1, 1),
-                                           padding="same")
-        self.conv_layer2 = torch.nn.Conv2d(in_channels=conv_out_channels,
-                                           out_channels=conv_out_channels,
-                                           kernel_size=conv_kernel_size,
-                                           stride=(1, 1),
-                                           padding="same")
+        if conv_type == "basic":
+            self.conv_layer1 = torch.nn.Conv2d(in_channels=conv_in_channels,
+                                               out_channels=conv_out_channels,
+                                               kernel_size=(3, 3),
+                                               stride=(1, 1),
+                                               padding="same")
+            self.conv_layer2 = torch.nn.Conv2d(in_channels=conv_out_channels,
+                                               out_channels=conv_out_channels,
+                                               kernel_size=(3, 3),
+                                               stride=(1, 1),
+                                               padding="same")
+        elif conv_type == "time_shifted":
+            self.conv_layer1 = TSConv(input_channels=conv_in_channels,
+                                      output_channels=conv_out_channels)
+            self.conv_layer2 = TSConv(input_channels=conv_out_channels,
+                                      output_channels=conv_out_channels)
+        else:
+            raise NotImplementedError("""Right now only two types of convolution layers 
+                                      are supported 'basic' and 'time_shifted'.
+                                      Read the class docstring for more info.""")
 
     def __call__(self, x , y):
         x = self.upsample(x)
@@ -43,3 +63,21 @@ class UpsampleBlock:
         x = torch.nn.ReLU()(self.conv_layer2(x))
         return x
 
+
+class TSConv(torch.nn.Module):
+    def __init__(self, input_channels, output_channels):
+        super(TSConv, self).__init__()
+        self.conv_layer1 = torch.nn.Conv2d(in_channels=input_channels,
+                                           out_channels=output_channels,
+                                           kernel_size=(3, 1),
+                                           stride=(1, 1),
+                                           padding="same")
+        
+    def forward(self, x):
+        x_dynamic = x[:, :x.shape[1] // 2, :, :]
+        x_dynamic_forward = torch.concat([torch.zeros(1, (x_dynamic.shape[1] // 2), x_dynamic.shape[2], 1), x_dynamic[:, :(x_dynamic.shape[1] // 2), :, :-1]], dim=-1)
+        x_dynamic_backward = torch.concat([x_dynamic[:, (x_dynamic.shape[1] // 2):, :, 1:], torch.zeros(1, (x_dynamic.shape[1] // 2), x_dynamic.shape[2], 1)], dim=-1)
+        x = torch.concat([x_dynamic_forward, x_dynamic_backward, x[:, (x.shape[1] // 2):, :, :]], dim=1)
+        x = self.conv_layer1(x)
+        return x 
+    
